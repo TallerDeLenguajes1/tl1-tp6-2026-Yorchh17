12dcc89ee4ea0099b78d22e058b8bfc0b833553f
Jose <jorgejose331gmail.com>
2026-05-25 22:37:33 -0300

    tp terminado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);*/
 
-Console.WriteLine("Ingrese un numero para invertir:");
+/*Console.WriteLine("Ingrese un numero para invertir:");
 string s =Console.ReadLine();
 int num=0;
 
@@ -32,7 +32,57 @@ int num=0;
     {
     Console.WriteLine("Error: Lo que ingresaste no es un número válido.");
     }
+*/
+//● Obtener la longitud de la cadena y muestre por pantalla.
 
+Console.WriteLine("Ingrese una cadena de texto");
+string? texto = Console.ReadLine();
+if (texto != null)
+{
+    int longitud = texto.Length;
+    Console.WriteLine("\nLongitud de la cadena: " + longitud);
+}
+Console.WriteLine("\nIngrese una segunda cadena de texto");
+string? texto2 = Console.ReadLine();
+if (texto != null && texto2 != null)
+{
+    // Concatenar cadenas
+    string textoCompleto = texto + " " + texto2;
+    Console.WriteLine("\nCadenas concatenadas: " + textoCompleto);
+    // Subcadena
+    string subcadena = texto.Substring(0, 3);
+    Console.WriteLine("\nSubcadena: " + subcadena);
+    foreach(char caracter in textoCompleto)
+    {
+        Console.WriteLine("\nCaracter: "+caracter);
+    };
+    Console.WriteLine("\nIngrese la palabra a buscar ");
+    string? buscada = Console.ReadLine();
+    int contador = 0;
+    if(buscada != null)
+    {
+        string[] palabras = textoCompleto.Split(' ');
+        foreach(string palabra in palabras)
+        {
+            if (buscada == palabra)
+            {
+                contador++;
+            }
+            
+        }
+        Console.WriteLine("\nOcurrencias: "+contador);
+        Console.WriteLine("\nCadena completa MAYUS: "+ (textoCompleto.ToUpper()));
+        Console.WriteLine("\nCadena completa MAYUS: "+ (textoCompleto.ToLower()));
+    }
+}
 
-
-
+Console.WriteLine("\nIngrese una oracion usando como separador '/'");
+string? texto3 = Console.ReadLine();
+if(texto3 != null)
+{
+    string[] partes = texto3.Split("/");
+    foreach(string parte in partes)
+    {
+        Console.WriteLine("Palabra: "+parte);
+    }
+}
